5bcce0c437ff93145a109f9677de2ceaff8cdb6e
David Portilla <david_portilla@hotmail.com>
2021-06-21 17:09:15 -0500

    adding state for projects

--- client/src/components/projects/NewProject.js
+++ client/src/components/projects/NewProject.js
@@ -1,17 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
 
 const NewProject = () => {
+  const [project, setProject] = useState({
+    name: "",
+  });
+
+  const { name } = project;
+
+  const onChange = (e) => {
+    setProject({
+      ...project,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+
   return (
     <>
       <button type="button" className="btn btn-block btn-primario">
         Add new project
       </button>
-      <form className="formulario-nuevo-proyecto">
+      <form onSubmit={handleSubmit} className="formulario-nuevo-proyecto">
         <input
           type="text"
           className="input-text"
-          name="nombre"
+          name="name"
+          value={name}
           placeholder="Project name"
+          onChange={onChange}
         />
         <input
           type="submit"
