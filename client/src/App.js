f36081775850279030f56baef7796d40a143b94d
David Portilla <david_portilla@hotmail.com>
2021-06-24 15:06:52 -0500

    adding Project state and context

--- client/src/App.js
+++ client/src/App.js
@@ -3,17 +3,20 @@ import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Login from "./components/auth/Login";
 import NewAccount from "./components/auth/NewAccount";
 import Projects from "./components/projects/Projects";
+import ProjectState from "./context/projects/projectState";
 
 function App() {
   return (
-    <Router>
-      <Switch>
-        Login
-        <Route exact path="/" component={Login} />
-        <Route exact path="/register" component={NewAccount} />
-        <Route exact path="/projects" component={Projects} />
-      </Switch>
-    </Router>
+    <ProjectState>
+      <Router>
+        <Switch>
+          Login
+          <Route exact path="/" component={Login} />
+          <Route exact path="/register" component={NewAccount} />
+          <Route exact path="/projects" component={Projects} />
+        </Switch>
+      </Router>
+    </ProjectState>
   );
 }
 
---
+++ client/src/context/projects/projectState.js
@@ -0,0 +1,22 @@
+import { useReducer } from "react";
+import projectContext from "./projectContext";
+import projectReducer from "./projectReducer";
+
+const ProjectState = (props) => {
+  const initialState = {
+    formProject: false,
+  };
+  const [state, dispatch] = useReducer(projectReducer, initialState);
+
+  return (
+    <projectContext.Provider
+      value={{
+        formProject: state.formProject,
+      }}
+    >
+      {props.children}
+    </projectContext.Provider>
+  );
+};
+
+export default ProjectState;
