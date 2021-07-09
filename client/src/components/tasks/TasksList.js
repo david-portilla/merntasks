c85f82e0b629ab3294f680d3086902564de9a207
David Portilla <david_portilla@hotmail.com>
2021-07-09 17:11:45 -0500

    getting current name project

--- client/src/components/tasks/TasksList.js
+++ client/src/components/tasks/TasksList.js
@@ -1,6 +1,13 @@
+import React, { useContext } from "react";
 import Task from "./Task";
+import projectContext from "../../context/projects/projectContext";
 
 const TasksList = () => {
+  const projectsContext = useContext(projectContext);
+  const { selectedProject } = projectsContext;
+  if(!selectedProject) return <h2>Select a project from sidebar</h2>
+  const [currentProject] = selectedProject;
+
   const tasks = [
     { name: "Choose platform", status: true },
     { name: "Select colors", status: false },
@@ -10,7 +17,7 @@ const TasksList = () => {
 
   return (
     <>
-      <h2>Project: Ecommerce</h2>
+      <h2>Project: {currentProject.name}</h2>
       <ul className="listado-tareas">
         {tasks.length === 0 ? (
           <li className="tarea">
